4e353542a4fc86989e606178b2106b220975d383
Maaike Sloot <msloot@paul-f4Br2s2.clusters.42paris.fr>
2023-09-11 11:27:07 +0200

    ex02

--- c01/ex02/ft_swap.c
+++ c01/ex02/ft_swap.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msloot <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/09 19:31:59 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/09 19:42:30 by msloot           ###   ########.fr       */
+/*   Created: 2023/09/11 10:39:17 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/11 11:26:23 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 void	ft_swap(int *a, int *b)
 {
+	int	tmp;
 
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
 }
+
+/*
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	a = 42;
+	b = 21;
+	ft_swap(&a, &b);
+	printf("%d %d", a, b);
+}
+*/
