55f3ed585e9ccc58e36ab80f65ca1e2aca0be09c
Maaike Sloot <msloot@paul-f4Br2s2.clusters.42paris.fr>
2023-09-11 19:46:07 +0200

    ex05

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 18:04:06 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/11 19:43:44 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
+
+int	main(void)
+{
+	ft_putstr("hello");
+}
