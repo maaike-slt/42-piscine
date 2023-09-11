61676a8e812d44c87eb6e02ca3eabdccdaa01e0e
Maaike Sloot <msloot@paul-f4Br2s2.clusters.42paris.fr>
2023-09-11 12:27:36 +0200

    ex03

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -1,22 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 10:39:17 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/11 11:37:21 by msloot           ###   ########.fr       */
+/*   Created: 2023/09/11 11:33:10 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/11 12:27:03 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_swap(int *a, int *b)
-{
-	int	tmp;
 
-	tmp = *b;
-	*b = *a;
-	*a = tmp;
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
 }
 
 /*
@@ -24,10 +22,12 @@ int	main(void)
 {
 	int	a;
 	int	b;
+	int	div;
+	int	mod;
 
 	a = 42;
-	b = 21;
-	ft_swap(&a, &b);
-	printf("%d %d", a, b);
+	b = 9;
+	ft_div_mod(a, b, &div, &mod);
+	printf("%d %d %d %d", a, b, div, mod);
 }
 */
