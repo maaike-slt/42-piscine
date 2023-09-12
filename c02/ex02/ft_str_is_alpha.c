c38f479d6b4e86fd365a0f7ca334d5ce692dd28a
maaike-slt <maaikesloot@home.nl>
2023-09-12 23:06:51 +0200

    ex02

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 22:38:03 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/12 23:06:09 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z')
+				|| (str[i] >= 'A' && str[i] <= 'Z')))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/*
+int	main(void)
+{
+	char	c;
+
+	c = ft_str_is_alpha("pizza") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_alpha("piz42za") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_alpha("") + '0';
+	write(1, &c, 1);
+}
+*/
