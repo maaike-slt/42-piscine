0286896dea29fed82ef8b61cd8995b0b37b4e9f5
maaike-slt <maaikesloot@home.nl>
2023-09-12 22:01:41 +0200

    fixed buggy

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -6,39 +6,40 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 12:20:10 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/12 18:59:19 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/12 22:00:31 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include <unistd.h>
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	int	k;
+	size_t	i;
 
-	k = '0';
-	while (src[k] != '\0')
+	i = 0;
+	while (src[i] != '\0')
 	{
-		dest[k] = src[k];
-		k++;
+		dest[i] = src[i];
+		i++;
 	}
-	dest[k] = '\0';
-	return(dest);
+	dest[i] = '\0';
+	return (dest);
 }
 
+/*
 int	main(void)
 {
-	char	dest[5];
-	char	src[5];
+	char	dest[42];
+	char	src[42];
 
 	src[0] = '\0';
 	dest[0] = '\0';
 
-	printf("%s %s\n", src, dest);
+	printf("'%s' '%s'\n", src, dest);
 
 	ft_strcpy(src, "pizza");
 	ft_strcpy(dest, src);
 
-	printf("%s %s\n", src, dest);
+	printf("'%s' '%s'\n", src, dest);
 }
+*/
