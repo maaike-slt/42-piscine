2d14e02f3e032a75427b673f4ff061ff7f7f6b7a
Maaike Sloot <msloot@paul-f3Ar3s9.clusters.42paris.fr>
2023-09-13 13:46:08 +0200

    ex05

---
+++ c02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 13:11:36 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/13 13:45:21 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'A' && str[i] <= 'Z'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+#include <unistd.h>
+
+int	main(void)
+{
+	char	c;
+
+	c = ft_str_is_uppercase("PIZZA") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_uppercase("piz42za") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_uppercase("PIz42ZA") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_uppercase("") + '0';
+	write(1, &c, 1);
+}
