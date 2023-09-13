f4ed2411ee7223016ecd7810552424463994c307
Maaike Sloot <msloot@paul-f3Ar3s9.clusters.42paris.fr>
2023-09-13 12:50:48 +0200

    ex03

---
+++ c02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 12:28:43 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/13 12:49:52 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+#include <unistd.h>
+
+int	main(void)
+{
+	char	c;
+
+	c = ft_str_is_numeric("85670349") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_numeric("012d345") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_numeric("") + '0';
+	write(1, &c, 1);
+}
