ad72c9ab10453d581fd6759937dd12b76e13d49b
Maaike Sloot <msloot@paul-f3Ar3s9.clusters.42paris.fr>
2023-09-13 16:48:41 +0200

    ex06

---
+++ c02/ex06/ft_str_is_printable.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 15:42:15 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/13 15:51:33 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_str_is_printable(char *str)
+	{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 32 && str[i] <= 126))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char	c;
+
+	c = ft_str_is_printable("pizza") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_printable("piz42\nza") + '0';
+	write(1, &c, 1);
+	c = ft_str_is_printable("") + '0';
+	write(1, &c, 1);
+}
