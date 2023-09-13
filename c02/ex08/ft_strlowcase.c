662848b519dc99f47b3fe8084b36db98ad70b4d8
Maaike Sloot <msloot@paul-f3Ar3s9.clusters.42paris.fr>
2023-09-13 19:02:02 +0200

    ex08

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 18:48:16 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/13 18:58:15 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	str[] = "hEl42Lo";
+	printf("%s", ft_strlowcase(str));
+}
