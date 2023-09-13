15fcd19955c51f55e2b68ac6b85e4374638a3285
Maaike Sloot <msloot@paul-f3Ar3s9.clusters.42paris.fr>
2023-09-13 17:54:53 +0200

    ex07

---
+++ c02/ex07/ft_strupcase.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 16:53:35 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/13 17:53:40 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	char str[] = "hEl42Lo";
+//	ft_strupcase("hElLo");
+//	printf("%s", str);
+	printf("%s", ft_strupcase(str)); 
+}
