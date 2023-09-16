49d64e1bc8064d9e0042cc8e0c37c55f0097a904
maaike-slt <maaikesloot@home.nl>
2023-09-16 08:58:50 +0200

    ex03

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 19:40:29 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/13 22:49:28 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/13 23:02:50 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (i < n && s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	if (s1[i] == '\0' && s2[i] == '\0')
+	if (i == n || (s1[i] == '\0' && s2[i] == '\0'))
 		return (0);
 	return (s1[i] - s2[i]);
 }
@@ -31,12 +31,15 @@ int	ft_strcmp(char *s1, char *s2)
 
 int	main(void)
 {
-	printf("%d\n", ft_strcmp("pizza", "pizza"));
-	printf("%d\n", ft_strcmp("pizza", "pozza"));
-	printf("%d\n", ft_strcmp("pizza", "pizzapoo"));
-	printf("%d\n", ft_strcmp("azzipizza", "pizza"));
-	printf("%d\n", ft_strcmp("", ""));
-	printf("%d\n", ft_strcmp("A", ""));
-	printf("%d\n", ft_strcmp("", "A"));
+	printf("%d\n", ft_strncmp("pizza", "pizza", 5));
+	printf("%d\n", ft_strncmp("pizza", "pozza", 5));
+	printf("%d\n", ft_strncmp("pizza", "pizzapoo", 42));
+	printf("%d\n", ft_strncmp("azzipizza", "pizza", 42));
+	printf("%d\n", ft_strncmp("", "", 0));
+	printf("%d\n", ft_strncmp("", "", 1));
+	printf("%d\n", ft_strncmp("A", "", 1));
+	printf("%d\n", ft_strncmp("", "A", 1));
+	printf("%d\n", ft_strncmp("pizza", "pizza", 3));
+	printf("%d\n", ft_strncmp("pizza", "pizzo", 4));
 }
 */
