14ce23adce7dd3f9dda66f0241b395513ecfd895
maaike-slt <maaikesloot@home.nl>
2023-09-17 23:14:00 +0200

    ex05

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 13:58:24 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/17 14:00:36 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/17 23:12:35 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,50 @@ unsigned int	ft_strlen(char *str)
 	return (i);
 }
 
-unsigned int:wq
-ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	src_i;
+	unsigned int	dest_i;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	dest_i = dest_len;
+	src_i = 0;
+	if (dest_len < size - 1 && size > 0)
+	{
+		while (dest_len + src_i < size - 1 && src[src_i] != '\0')
+		{
+			dest[dest_i] = src[src_i];
+			dest_i++;
+			src_i++;
+		}
+		dest[dest_i] = '\0';
+	}
+	if (dest_len > size)
+		dest_len = size;
+	return (dest_len + src_len);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	src[42] = "pizza";
+	char	dest[42] = "coconut";
+
+	printf("%s: %u\n", src, ft_strlen(src));
+	printf("%s: %u\n", dest, ft_strlen(dest));
+
+	printf("%u\n", ft_strlcat(dest, src, 21));
+
+	printf("%s: %u\n", src, ft_strlen(src));
+	printf("%s: %u\n", dest, ft_strlen(dest));
+
+	printf("%u\n", ft_strlcat(src, "no", 21));
+	printf("%u\n", ft_strlcat(dest, src, 16));
+
+	printf("%s: %u\n", src, ft_strlen(src));
+	printf("%s: %u\n", dest, ft_strlen(dest));
+}
