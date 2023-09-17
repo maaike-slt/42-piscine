77e4554e7cc120f788194b2d49dbad0b8ea973e2
maaike-slt <maaikesloot@home.nl>
2023-09-17 22:57:04 +0200

    ex03 no buggy

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 16:33:56 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/17 17:14:47 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/17 19:45:22 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,57 @@ int	ft_atoi(char *str)
 	int	nb;
 
 	i = 0;
-	sign = 0;
+	sign = 1;
 	nb = 0;
-	while ((str[i] > 8 && str[i] < 14) || str[i] == 32)
+	while ((str[i] > 8 && str[i] < 14) || str[i] == ' ')
 		i++;
 	while (str[i] == '+' || str[i] == '-')
-		sign = -sign;
-	i++;
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
 	while (str[i] != '\0' && ft_isnum(str[i]))
 	{
 		nb = nb * 10;
-		nb = nb(str[i] - '0');
+		nb += str[i] - '0';
 		i++;
 	}
 	return (sign * nb);
 }
 
+/*
+#include <stdlib.h>
+
 int	main(void)
 {
+	printf("%d\n", ft_atoi("+42"));
+	printf("%d\n", ft_atoi("-42"));
+	printf("%d\n", ft_atoi("-+-42"));
+	printf("%d\n", ft_atoi("+-+42"));
+	printf("%d\n", ft_atoi(" ---+--+1234ab567"));
 	printf("%d\n", ft_atoi(" 	 -+--++------672"));
+	printf("%d\n", ft_atoi("penapplepen"));
+	printf("%d\n", ft_atoi("-penapplepen"));
+	printf("%d\n", ft_atoi("-"));
+	printf("%d\n", ft_atoi(" 42 42"));
+	printf("%d\n", ft_atoi("\f42"));
+	printf("%d\n", ft_atoi("\n42"));
+	printf("%d\n", ft_atoi("\r42"));
+	printf("%d\n", ft_atoi("\t42"));
+	printf("%d\n", ft_atoi("\v42"));
+	printf("%d\n", ft_atoi("a42-42"));
+	printf("%d\n", ft_atoi("2147483647") == atoi("2147483647"));
+	printf("%d\n", ft_atoi("-2147483648") == atoi("-2147483648"));
+	printf("%d\n", ft_atoi("2147483648") == atoi("2147483648"));
+	printf("%d\n", ft_atoi("-2147483649") == atoi("-2147483649"));
+	printf("%d\n", ft_atoi("4147483648") == atoi("4147483648"));
+	printf("%d\n", ft_atoi("-4147483649") == atoi("-4147483649"));
+	printf("%d\n", ft_atoi("2147483647"));
+	printf("%d\n", ft_atoi("-2147483648"));
+	printf("%d\n", ft_atoi("2147483648"));
+	printf("%d\n", ft_atoi("-2147483649"));
+	printf("%d\n", ft_atoi("4147483648"));
+	printf("%d\n", ft_atoi("-4147483649"));
 }
+*/
