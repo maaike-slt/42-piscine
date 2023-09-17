ee29a4228803cecd8ce9848ba318e3de0c810d61
Maaike Sloot <msloot@bess-f2r1s16.clusters.42paris.fr>
2023-09-17 16:18:48 +0200

    start ex05

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 13:58:24 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/17 14:00:36 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int:wq
+ft_strlcat(char *dest, char *src, unsigned int size)
