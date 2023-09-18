f60ec8ea33761b1a859bc692baddd3027bbdba37
Maaike Sloot <msloot@bess-f3r3s8.clusters.42paris.fr>
2023-09-18 15:20:26 +0200

    fix

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 13:17:34 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/18 13:28:43 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/18 15:19:34 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,10 @@ char	*ft_strstr(char *str, char *to_find)
 	unsigned int	i;
 	unsigned int	to_find_len;
 
-	i = 0;
-	to_find_len = ft_strlen(to_find);
 	if (to_find[0] == '\0')
 		return (&str[0]);
+	i = 0;
+	to_find_len = ft_strlen(to_find);
 	while (str[i] != '\0')
 	{
 		if (ft_strncmp(&str[i], to_find, to_find_len) == 0)
