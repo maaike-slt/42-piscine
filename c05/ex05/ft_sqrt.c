29ed4767e454ff7cacd409473bb5d626f1ab7993
maaike-slt <maaikesloot@home.nl>
2023-09-19 22:13:57 +0200

    ex05

---
+++ c05/ex05/ft_sqrt.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 21:48:57 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/19 22:09:53 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_sqrt(int nb)
+{
+	int	i;
+
+	i = 1;
+	if (nb < 1)
+		return (0);
+	while (i * i < nb && i < 46341)
+		i++;
+	if (i * i == nb)
+		return (i);
+	return (0);
+}
+
+#include <stdio.h>
+#include <limits.h>
+
+int	main(void)
+{
+	printf("%d\n", ft_sqrt(-1));
+	printf("%d\n", ft_sqrt(0));
+	printf("%d\n", ft_sqrt(1));
+	printf("%d\n", ft_sqrt(2));
+	printf("%d\n", ft_sqrt(3));
+	printf("%d\n", ft_sqrt(4));
+	printf("%d\n", ft_sqrt(5));
+	printf("%d\n", ft_sqrt(36));
+	printf("%d\n", ft_sqrt(46340 * 46340));
+	// printf("%d\n", ft_sqrt(46341 * 46341));
+	printf("%d\n", ft_sqrt(INT_MAX));
+	printf("%d\n", ft_sqrt(INT_MIN));
+}
