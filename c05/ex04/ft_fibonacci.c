177f7819da714487112dfa236b728e1905178af9
maaike-slt <maaikesloot@home.nl>
2023-09-19 21:47:52 +0200

    ex04 fibbo

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 21:32:11 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/19 21:47:10 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index < 2)
+		return (index);
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d\n", ft_fibonacci(i));
+		i++;
+	}
+}
