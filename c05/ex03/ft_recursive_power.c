ecad7f6df3c0409413e6f2984c81a5a51a493fa6
maaike-slt <maaikesloot@home.nl>
2023-09-19 21:31:28 +0200

    ex02 ex03

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -1,46 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 15:38:36 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/19 21:29:58 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/19 21:28:43 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_iterative_power(int nb, int power)
+int	ft_recursive_power(int nb, int power)
 {
-	int	pow;
-
-	pow = 1;
 	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
-	while (power > 0)
-	{
-		pow *= nb;
-		power--;
-	}
-	return (pow);
+	return (nb * ft_recursive_power(nb, power - 1));
 }
 
 int	main(void)
 {
-	printf("%d\n", ft_iterative_power(5, 3));
-	printf("%d\n", ft_iterative_power(-5, 3));
-	printf("%d\n", ft_iterative_power(-5, 2));
-	printf("%d\n", ft_iterative_power(5, -3));
-	printf("%d\n", ft_iterative_power(0, 0));
-	printf("%d\n", ft_iterative_power(1, 0));
-	printf("%d\n", ft_iterative_power(5, 0));
-	printf("%d\n", ft_iterative_power(0, 1));
-	printf("%d\n", ft_iterative_power(0, 5));
-	printf("%d\n", ft_iterative_power(1, 5));
-	printf("%d\n", ft_iterative_power(5, 1));
-	printf("%d\n", ft_iterative_power(42, 21));
+	printf("%d\n", ft_recursive_power(5, 3));
+	printf("%d\n", ft_recursive_power(-5, 3));
+	printf("%d\n", ft_recursive_power(-5, 2));
+	printf("%d\n", ft_recursive_power(5, -3));
+	printf("%d\n", ft_recursive_power(0, 0));
+	printf("%d\n", ft_recursive_power(1, 0));
+	printf("%d\n", ft_recursive_power(5, 0));
+	printf("%d\n", ft_recursive_power(0, 1));
+	printf("%d\n", ft_recursive_power(0, 5));
+	printf("%d\n", ft_recursive_power(1, 5));
+	printf("%d\n", ft_recursive_power(5, 1));
+	printf("%d\n", ft_recursive_power(42, 21));
 }
