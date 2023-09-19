6f226bf0998e8a5b552cba3a1d07710cf4f5aa3f
maaike-slt <maaikesloot@home.nl>
2023-09-19 22:37:46 +0200

    ex07

---
+++ c05/ex07/ft_find_next_prime.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 22:14:22 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/19 22:36:41 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 3;
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb <= 2)
+		return (2);
+	if (nb % 2 == 0)
+		nb++;
+	while (!ft_is_prime(nb))
+		nb += 2;
+	return (nb);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i;
+
+	i = -1;
+	while (i < 15)
+	{
+		printf("%d:\t%d\n", i, ft_find_next_prime(i));
+		i++;
+	}
+}
