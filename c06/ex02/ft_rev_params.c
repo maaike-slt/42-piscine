a1d0e521594e0bef321633130f75360a2668aae7
Maaike Sloot <msloot@paul-f3Ar4s6.clusters.42paris.fr>
2023-09-20 14:59:37 +0200

    ex02

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 12:03:44 by msloot            #+#    #+#             */
+/*   Updated: 2023/09/20 14:56:44 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc <= 1)
+		return (0);
+	while (argc > 1)
+	{
+		argc--;
+		ft_putstr(argv[argc]);
+		write(1, "\n", 1);
+	}
+}
