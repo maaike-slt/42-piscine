81e2ff66655e53e7a147e52d917b4ac9feee6864
maaike-slt <maaikesloot@home.nl>
2023-09-25 20:25:30 +0200

    ex11

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -6,17 +6,29 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 12:08:44 by msloot            #+#    #+#             */
-/*   Updated: 2023/09/21 13:53:25 by msloot           ###   ########.fr       */
+/*   Updated: 2023/09/25 20:22:06 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 int	ft_char_is_printable(char c)
 {
 	if (!(c >= 32 && c <= 126))
 		return (0);
 	return (1);
+}
+
+void	ft_puthex(char c)
+{
+	ft_putchar('\\');
+	ft_putchar("0123456789abcdef"[c / 16]);
+	ft_putchar("0123456789abcdef"[c % 16]);
 }
 
 void	ft_putstr_non_printable(char *str)
@@ -27,9 +39,9 @@ void	ft_putstr_non_printable(char *str)
 	while (str[i] != '\0')
 	{
 		if (ft_char_is_printable(str[i]))
-			write(1, &str[i], 1);
+			ft_putchar(str[i]);
 		else
-			write(1, "#", 1);
+			ft_puthex(str[i]);
 		i++;
 	}
 }
@@ -42,7 +54,8 @@ int	main(void)
 	write(1, "\n", 1);
 	ft_putstr_non_printable("pizza");
 	write(1, "\n", 1);
-	ft_putstr_non_printable("\0\t\r\n");
+	ft_putstr_non_printable("\v\t\r\n");
 	write(1, "\n", 1);
 	ft_putstr_non_printable("");
+	write(1, "\n", 1);
 }
